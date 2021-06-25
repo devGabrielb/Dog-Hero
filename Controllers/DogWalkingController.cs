937a3b57e01d5fcdef87d3ec7d6f9f1be9d93584
devGabrielb <dev.gabrielb1@gmail.com>
2021-06-25 17:53:47 -0300

    feat: add DogWalkingController

---
+++ Controllers/DogWalkingController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogHero.API.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/")]
+    public class DogWalkingController : ControllerBase
+    {
+        
+    }
+}
